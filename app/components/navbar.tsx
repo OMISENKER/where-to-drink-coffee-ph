3607dccb6bd71fb489dd8f8fd723bbf026105723
dominiquealvarez8888@gmail.com <dominiquealvarez8888@gmail.com>
2025-02-08 00:30:18 +0800

    upgraded to tailwindcss v4.2

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -5,19 +5,26 @@ import { Button } from "@/components/ui/button";
 
 const navbar = () => {
   return (
-    <div className="top-0 h-24 w-full flex justify-end items-center gap-10 px-10 bg-gray-800 text-white font-nunito">
-      <Link href="/" className="font-sansita">
-        Home
-      </Link>
-      <Link href="/trending">Trending</Link>
-      <form
-        action={async () => {
-          "use server";
-          await signIn("facebook");
-        }}
-      >
-        <Button type="submit">Sign in with Facebook</Button>
-      </form>
+    <div className="top-0 h-24 w-full flex items-center justify-between gap-10 px-10 bg-gray-800 text-white ">
+      <div>
+        <Link href={"/"} className="font-sansita text-3xl">
+          Where to Drink Coffee PH
+        </Link>
+      </div>
+      <div className="flex gap-10 items-center text-xl">
+        <Link href="/" className="font-nunito">
+          Home
+        </Link>
+        <Link href="/trending">Trending</Link>
+        <form
+          action={async () => {
+            "use server";
+            await signIn("facebook");
+          }}
+        >
+          <Button type="submit">Sign in with Facebook</Button>
+        </form>
+      </div>
     </div>
   );
 };
